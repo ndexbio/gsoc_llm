29e0ecc7a4b70c62b70c1eb8ac3bc27d4a4434ee
Favour James <favour.ujames196@gmail.com>
2024-05-13 15:46:21 +0100

    setting up development environment

---
+++ convert_to_cx2.py
@@ -0,0 +1,44 @@
+import json
+
+
+def convert_to_cx2(extracted_data):
+    # Initialize the CX2 format structure
+    cx2_network = {
+        "nodes": [],
+        "edges": [],
+        "networkAttributes": [],
+        "nodeAttributes": [],
+        "edgeAttributes": []
+    }
+
+    # Define node and edge ids
+    node_id = 1
+    edge_id = 1
+
+    # Maps to keep track of nodes and ensure they're unique
+    nodes_map = {}
+
+    # Process each interaction
+    for interaction in extracted_data:
+        for entity in interaction['entities_involved']:
+            if entity not in nodes_map:
+                nodes_map[entity] = node_id
+                cx2_network['nodes'].append({"@id": node_id, "n": entity})
+                node_id += 1
+
+        source_node_id = nodes_map[interaction['entities_involved'][0]]
+        target_node_id = nodes_map[interaction['entities_involved'][1]]
+
+        # Add edge based on the interaction
+        cx2_network['edges'].append({"@id": edge_id, "s": source_node_id, "t": target_node_id, "i": interaction['interaction_type']})
+        edge_id += 1
+
+        # Optionally, add edge attributes if interaction_details are present
+        if interaction.get('interaction_details'):
+            cx2_network['edgeAttributes'].append({
+                "po": edge_id - 1,          #property_of edge attribute
+                "n": "interaction_details",
+                "v": interaction['interaction_details']
+            })
+
+    return cx2_network
---
+++ model.py
@@ -0,0 +1,34 @@
+import os
+import warnings
+from dotenv import load_dotenv
+from typing import List, Optional
+from pydantic import BaseModel, Field
+from langchain_core.utils.function_calling import convert_pydantic_to_openai_function
+from langchain_openai import ChatOpenAI
+warnings.filterwarnings("ignore")
+
+
+OPENAI_API_KEY = os.environ.get("OPENAI_API_KEY")
+
+model = ChatOpenAI(model = "gpt-3.5-turbo", temperature=0, openai_api_key=OPENAI_API_KEY)
+
+# Define Pydantic models for the data we want to extract
+class Interaction(BaseModel):
+    """Information about molecular interactions mentioned."""
+    entities_involved: List[str]
+    interaction_type: str
+    interaction_details: Optional[str]
+
+class Molecular_Interactions(BaseModel):
+    """Information to extract."""
+    interactions: List[Interaction]
+    
+# Convert the pydantic classes to openai model   
+paper_extraction_function = [
+    convert_pydantic_to_openai_function(Molecular_Interactions)
+]
+extraction_model = model.bind(
+    functions=paper_extraction_function,
+    function_call={"name":"Molecular_Interactions"}
+)
+
