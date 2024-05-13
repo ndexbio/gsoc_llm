abdc7860edccb920f997bde230de55fb5ec07504
Favour James <favour.ujames196@gmail.com>
2024-05-13 19:09:55 +0100

    testing code with review paper

--- python scripts/chunking_test.py
+++ python scripts/chunking_test.py
@@ -1,11 +1,16 @@
-from read_pdf import read_pdf
 import json
+import time
+from read_pdf import read_pdf
 from get_interactions import extraction_chain
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.schema.runnable import RunnableLambda
 
+
+# Start the timer
+start_time = time.time()
+
 doc = read_pdf("/Users/favourjames/Downloads/gsoc_llm/papers/review_paper1.pdf")
-page_content = doc[50:50000]
+page_content = doc
 
 
 # Define a function to flatten a 2D list (matrix) into a 1D list (flat list).
@@ -19,13 +24,27 @@ def flatten(matrix):
 
 text_splitter = RecursiveCharacterTextSplitter(chunk_overlap=0)
 splits = text_splitter.split_text(page_content)
-
 prep = RunnableLambda(
     lambda x: [{"input": doc} for doc in text_splitter.split_text(x)]
 )
 
 chain = prep | extraction_chain.map() | flatten
-
 results = chain.invoke(page_content)
+
+# Initialize a set to store unique interaction types
+unique_interaction_types = set()
+for interaction in results:
+    if 'interaction_type' in interaction:
+        unique_interaction_types.add(interaction['interaction_type'])
+print(f"Number of unique interaction types: {len(unique_interaction_types)}")
+
+
 json_output = json.dumps(results, indent=4)
-print(json_output)
+with open('results/SIRT1_PARP1/output3.json', 'w') as file:
+    file.write(json_output)
+
+
+end_time = time.time()
+elapsed_time = end_time - start_time
+elapsed_minutes = elapsed_time / 60
+print(f"Time taken: {elapsed_time:.2f} seconds ({elapsed_minutes:.2f} minutes)")
