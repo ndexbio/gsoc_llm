7c71f1b4decf399733c09b3a55e9d1c1f0d395d2
Favour James <favour.ujames196@gmail.com>
2024-05-23 15:34:18 +0100

    edit code

--- python_scripts/test.py
+++ python_scripts/test.py
@@ -6,7 +6,7 @@ from convert_to_cx2 import convert_to_cx2
 warnings.filterwarnings("ignore")
 
 
-doc = read_pdf("/Users/favourjames/Downloads/gsoc_llm/papers/review_paper1.pdf")
+doc = read_pdf("papers/pmc6044858.pdf")
 page_content = doc[50:30000]
 # print(page_content)
 
@@ -15,7 +15,7 @@ json_output = json.dumps(results, indent=4)
 print(json_output)
 
 # Convert extracted data to CX2 format
-cx2_network = convert_to_cx2(results)
+# cx2_network = convert_to_cx2(results)
 
 # # Convert CX2 data to JSON
 # cx2_json = json.dumps(cx2_network, indent=4)
