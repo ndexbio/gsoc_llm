6b10bb5fb30050c0988ee363475b8dd06b0115f9
Favour James <favour.ujames196@gmail.com>
2024-08-24 19:24:23 +0100

    editing readme

--- python_scripts/convert_to_cx2.py
+++ python_scripts/convert_to_cx2.py
@@ -1,7 +1,48 @@
-""" You can use File -> Import -> Network from File … and choose the cx2 file to open it in Cytoscape. or just drag a 
-cx2 file from your Finder window to the session pane of your Cytoscape instance. To open it in Cytoscape Web you can 
-use Data -> Import -> Network from File and choose a cx2 file from the file chooser. To Upload to NDEx, you have to use 
-curl or postman to either post the Json content in the payload to https://www.ndexbio.org/v3/networks , or upload the 
-file to that endpoint (it will be the multipart form data format on field “CXNetworkStream”)."""
+import pandas as pd
+import json
+from ndex2.cx2 import PandasDataFrameToCX2NetworkFactory
+from ndex2.client import Ndex2
+
+# Load your JSON data from file
+with open('results/pmc3898398/llm_combined50_results.json', 'r') as file:
+    json_data = json.load(file)
+
+# Initialize lists to store extracted data
+source_list = []
+target_list = []
+interaction_list = []
+
+# Extract data from JSON
+for entry in json_data:
+    combined_results = entry.get('Combined_Results', [])
+    for result in combined_results:
+        parts = result.split()
+        if len(parts) == 3:
+            source, interaction, target = parts
+            source_list.append(source)
+            target_list.append(target)
+            interaction_list.append(interaction)
+
+# Create a DataFrame
+df = pd.DataFrame({
+    'source': source_list,
+    'target': target_list,
+    'interaction': interaction_list
+})
+# print(df)
+
+factory = PandasDataFrameToCX2NetworkFactory()
+cx2_network = factory.get_cx2network(df, source_field='source', target_field='target', edge_interaction='interaction')
+# cx2_network.set_name('Set any name of your choice')
+
+net_cx = cx2_network.to_cx2()
+# Create an NDEx client instance with your credentials
+client = Ndex2(username='favour.ujames196@gmail.com', password='Fujames17')
+client.save_new_cx2_network(net_cx)
+
+
+new_network = json.dumps(cx2_network.to_cx2(), indent=2)
+with open('results/cx2.cx', 'w') as file:
+    file.write(new_network)
 
 
